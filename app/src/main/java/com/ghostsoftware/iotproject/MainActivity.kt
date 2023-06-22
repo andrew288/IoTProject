9285600fe3512fc7af652719b93e52344c0726a0
Andrew <ajacoboc@unsa.edu.pe>
2023-06-21 23:05:30 -0500

    Initial commit

---
+++ app/src/main/java/com/ghostsoftware/iotproject/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.ghostsoftware.iotproject
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import org.eclipse.paho.client.mqttv3.MqttClient
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
+import java.io.InputStream
+import java.security.KeyFactory
+import java.security.KeyStore
+import java.security.PrivateKey
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+import java.security.spec.PKCS8EncodedKeySpec
+import java.util.UUID
+import javax.net.ssl.KeyManagerFactory
+import javax.net.ssl.SSLContext
+
+class MainActivity : ComponentActivity() {
+    private val mqttServerUri = "ssl://a34bt8gk372w9w-ats.iot.us-east-2.amazonaws.com:8883"
+    private val clientId = UUID.randomUUID()
+    private val topic = "topic_test"
+    private val qos = 0
+
+    private lateinit var mqttClient: MqttClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val responseSubcribe = remember {
+                mutableStateOf("")
+            }
+                // A surface container using the 'background' color from the them
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Box(modifier = Modifier.fillMaxSize()){
+                        Text(text = responseSubcribe.value, color = Color.Black)
+                    }
+
+                }
+
+            try{
+                // Cargar el certificado de cliente desde el archivo
+                val certificateFactory = CertificateFactory.getInstance("X.509")
+                val certificateInputStream: InputStream = applicationContext.assets.open("cert.der")
+                val certificate = certificateFactory.generateCertificate(certificateInputStream) as X509Certificate
+
+                // Cargar la clave privada desde el archivo
+                val keyInputStream: InputStream = applicationContext.assets.open("private.der")
+                val privateKeyBytes = keyInputStream.readBytes()
+                val keyFactory = KeyFactory.getInstance("RSA")
+                val privateKeySpec = PKCS8EncodedKeySpec(privateKeyBytes)
+                val privateKey: PrivateKey = keyFactory.generatePrivate(privateKeySpec)
+
+                // Crear el contexto SSL
+                val keyStore = KeyStore.getInstance(KeyStore.getDefaultType())
+                keyStore.load(null)
+                keyStore.setCertificateEntry("alias", certificate)
+                keyStore.setKeyEntry("alias", privateKey, null, arrayOf(certificate))
+
+                val sslContext = SSLContext.getInstance("TLSv1.2")
+                val keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm())
+                keyManagerFactory.init(keyStore, null)
+                sslContext.init(keyManagerFactory.keyManagers, null, null)
+
+                // Configurar las opciones de conexión MQTT
+                val options = MqttConnectOptions()
+                options.socketFactory = sslContext.socketFactory
+
+                // Crear el cliente MQTT y establecer la conexión
+                mqttClient = MqttClient(mqttServerUri, clientId.toString(), MemoryPersistence())
+                mqttClient.connect(options)
+                mqttClient.subscribe(topic, qos) { _, message ->
+                    val luminosity = message.payload.toString(Charsets.UTF_8)
+                    Log.d("TEST-MQTT",luminosity)
+                    responseSubcribe.value = responseSubcribe.value  + "MESSAGE: " + luminosity
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mqttClient.disconnect()
+    }
+}
---
+++ app/src/main/java/com/ghostsoftware/iotproject/manager/MQTTClient.kt
@@ -0,0 +1,127 @@
+package com.ghostsoftware.iotproject.manager
+
+import android.content.Context
+import android.util.Log
+import org.eclipse.paho.android.service.MqttAndroidClient
+import org.eclipse.paho.client.mqttv3.IMqttActionListener
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
+import org.eclipse.paho.client.mqttv3.IMqttToken
+import org.eclipse.paho.client.mqttv3.MqttCallback
+import org.eclipse.paho.client.mqttv3.MqttException
+import org.eclipse.paho.client.mqttv3.MqttMessage
+
+class MQTTClient {
+    private lateinit var mqttAndroidClient: MqttAndroidClient
+
+    fun connect(applicationContext : Context) {
+        mqttAndroidClient = MqttAndroidClient ( applicationContext,"a34bt8gk372w9w-ats.iot.us-east-2.amazonaws.com","YOUR CLIENT ID" )
+        try {
+            val token = mqttAndroidClient.connect()
+            token.actionCallback = object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken)                        {
+                    Log.i("Connection", "success ")
+                    //connectionStatus = true
+                    // Give your callback on connection established here
+                }
+                override fun onFailure(asyncActionToken: IMqttToken, exception: Throwable) {
+                    //connectionStatus = false
+                    Log.i("Connection", "failure")
+                    // Give your callback on connection failure here
+                    exception.printStackTrace()
+                }
+            }
+        } catch (e: MqttException) {
+            // Give your callback on connection failure here
+            e.printStackTrace()
+        }
+    }
+
+    fun subscribe(topic: String) {
+        val qos = 2 // Mention your qos value
+        try {
+            mqttAndroidClient.subscribe(topic, qos, null, object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken) {
+                    // Give your callback on Subscription here
+                }
+                override fun onFailure(
+                    asyncActionToken: IMqttToken,
+                    exception: Throwable
+                ) {
+                    // Give your subscription failure callback here
+                }
+            })
+        } catch (e: MqttException) {
+            // Give your subscription failure callback here
+        }
+    }
+
+    fun unSubscribe(topic: String) {
+        try {
+            val unsubToken = mqttAndroidClient.unsubscribe(topic)
+            unsubToken.actionCallback = object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken) {
+                    // Give your callback on unsubscribing here
+                }
+                override fun onFailure(asyncActionToken: IMqttToken, exception: Throwable) {
+                    // Give your callback on failure here
+                }
+            }
+        } catch (e: MqttException) {
+            // Give your callback on failure here
+        }
+    }
+
+    fun receiveMessages() {
+        mqttAndroidClient.setCallback(object : MqttCallback {
+            override fun connectionLost(cause: Throwable) {
+                //connectionStatus = false
+                // Give your callback on failure here
+            }
+            override fun messageArrived(topic: String, message: MqttMessage) {
+                try {
+                    val data = String(message.payload, charset("UTF-8"))
+                    // data is the desired received message
+                    // Give your callback on message received here
+                } catch (e: Exception) {
+                    // Give your callback on error here
+                }
+            }
+            override fun deliveryComplete(token: IMqttDeliveryToken) {
+                // Acknowledgement on delivery complete
+            }
+        })
+    }
+    fun publish(topic: String, data: String) {
+        val encodedPayload : ByteArray
+        try {
+            encodedPayload = data.toByteArray(charset("UTF-8"))
+            val message = MqttMessage(encodedPayload)
+            message.qos = 2
+            message.isRetained = false
+            mqttAndroidClient.publish(topic, message)
+        } catch (e: Exception) {
+            // Give Callback on error here
+        } catch (e: MqttException) {
+            // Give Callback on error here
+        }
+    }
+    fun disconnect() {
+        try {
+            val disconToken = mqttAndroidClient.disconnect()
+            disconToken.actionCallback = object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken) {
+                    //connectionStatus = false
+                    // Give Callback on disconnection here
+                }
+                override fun onFailure(
+                    asyncActionToken: IMqttToken,
+                    exception: Throwable
+                ) {
+                    // Give Callback on error here
+                }
+            }
+        } catch (e: MqttException) {
+            // Give Callback on error here
+        }
+    }
+}
